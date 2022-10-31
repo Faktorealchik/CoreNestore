c9f533ce0e20f6dfd6efccc863e5b33c1a1939b2
aleksandr.nesterov <faktoreal4ik@gmail.com>
2022-10-31 10:47:02 +0800

    added

---
+++ Sources/CoreNestore/Core/DismissKeyboard/DismissKeyboardOnTap.swift
@@ -0,0 +1,68 @@
+//
+//  DismissKeyboardOnTap.swift
+//  
+//
+//  Created by Aleksandr Nesterov on 31.10.2022.
+//
+
+import SwiftUI
+
+public extension View {
+    func dismissKeyboardOnTap() -> some View {
+        modifier(DismissKeyboardOnTap())
+    }
+}
+
+public struct DismissKeyboardOnTap: ViewModifier {
+    public func body(content: Content) -> some View {
+        #if os(macOS)
+        return content
+        #else
+        return content.gesture(tapGesture)
+        #endif
+    }
+    
+    private var tapGesture: some Gesture {
+        TapGesture().onEnded(endEditing)
+    }
+    
+    private func endEditing() {
+        UIApplication.shared.connectedScenes
+            .filter {$0.activationState == .foregroundActive}
+            .map {$0 as? UIWindowScene}
+            .compactMap({$0})
+            .first?.windows
+            .filter {$0.isKeyWindow}
+            .first?.endEditing(true)
+    }
+}
+
+class KeyboardResponder: ObservableObject {
+    @Published var currentHeight: CGFloat = 0
+    
+    var _center: NotificationCenter
+    
+    init(center: NotificationCenter = .default) {
+        _center = center
+        _center.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        _center.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyBoardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            withAnimation {
+                currentHeight = keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyBoardWillHide(notification: Notification) {
+        withAnimation {
+            currentHeight = 0
+        }
+    }
+}
